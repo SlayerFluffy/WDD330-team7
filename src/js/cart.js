ebb829d7aa6fb74b7e5a4b2ef1ad661c07b80924
SlayerFluffy <wjezek@byupathway.edu>
2026-01-27 15:22:41 -0700

    Solving bugs in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,46 @@
-import { loadHeaderFooter } from './utils.mjs';
-import ShoppingCart from './ShoppingCart.mjs';
+import { loadHeaderFooter, getLocalStorage } from './utils.mjs';
 
 loadHeaderFooter();
 
-const cart = new ShoppingCart('so-cart', '.product-list');
-cart.init();
+function renderCartContents() {
+    const cartItems = getLocalStorage('so-cart');
+
+    if (!cartItems|| cartItems.length === 0) {
+        document.querySelector('.product-list').innerHTML = '<p>Your cart is empty</p>';
+        return;
+    }
+
+    const htmlTemplate = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector('.product-list').innerHTML = htmlTemplate.join('');
+
+    document.querySelector('.hide')?.classList.remove('hide');
+
+    document.querySelector('.cart-total').innerHTML = `Total: $${calculateTotal(cartItems).toFixed(2)}`;
+}
+
+function calculateTotal(list) {
+    const amounts = list.map((item) => item.FinalPrice);
+    let total = amounts.reduce((sum, item) => sum + item, 0);
+    return total;
+}
+
+function cartItemTemplate(item) {
+    const newItem = `<li class="cart-card divider">
+        <a href="#" class="cart-card__image">
+            <img
+            src="${item.Image}"
+            alt="${item.Name}"
+            />
+        </a>
+        <a href="#">
+            <h2 class="card__name">${item.Name}</h2>
+        </a>
+        <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+        <p class="cart-card__quantity">qty: 1</p>
+        <p class="cart-card__price">$${item.FinalPrice.toFixed(2)}</p>
+    </li>`;
+
+    return newItem;
+}
+
+renderCartContents();
